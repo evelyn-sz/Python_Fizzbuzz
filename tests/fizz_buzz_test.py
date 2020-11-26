710ff81d982535e0c5c4d12820ef6922e7c70de7
evelyn-sz <evelynszarek@gmail.com>
2020-11-26 11:48:52 +0000

    Test 3 passed

--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -8,3 +8,6 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz__5_returns_buzz(self):
         self.assertEqual("Buzz", fizz_buzz(5))
+
+    def test_fizz_buzz__15_returns_fizzbuzz(self):
+        self.assertEqual("FizzBuzz", fizz_buzz(15))
